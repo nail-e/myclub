32243b3a10463c00d11095cb0dfe474f69a63b54
nail-e <elianinigorieza@protonmail.com>
2022-09-19 21:21:03 +0400

    idk anymore

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -3,11 +3,23 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime("%B")):
     name = "Elian"
-    return render(request, 
-    "home.html", {
+    month = month.title()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    tc = calendar.HTMLCalendar()
+    cal = tc.formatmonth(year,month_number)
+    now = datetime.now()
+    current_year = now.year
+    time = now.strftime("%I:%M: %p")
+    
+    return render(request, "events/home.html", {
         "name": name,
         "year": year,
-        "month": month, 
-    })
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "time": time,
+        })
