86da1dd52b24b1f894cb365d94ccaeed3dd79dbb
Elian R <elianinigorieza@gmail.com>
2022-11-06 18:56:12 +0400

    Final commit

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -3,8 +3,8 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 
-#def home(request, year=datetime.now().year, month=datetime.now().strftime("%B")):
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime("%B")):
+#def home(request, year, month):
     name = "Elian"
     month = month.capitalize()
     month_number = list(calendar.month_name).index(month)
@@ -15,8 +15,8 @@ def home(request, year, month):
     current_year = now.year
     time = now.strftime("%I:%M: %p")
     
-    #return render(request, "events/home.html", {
-    return render(request, "home.html", {
+    return render(request, "events/home.html", {
+    #return render(request, "home.html", {
         "name": name,
         "year": year,
         "month": month,
